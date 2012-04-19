1f3f5516386121295896aac34d8625f5dd3e2c02
Felipe Knorr Kuhn <git@knorrium.info>
2012-04-19 15:58:40 -0300

    OO-fied the Client. Now parses a JSON file with client config

--- Yahoo/Messenger/Client.php
+++ Yahoo/Messenger/Client.php
@@ -25,142 +25,176 @@
  *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
 */
+require_once 'Engine.php';
 
-define('USERNAME', '');
-define('PASSWORD', '');
-define('CONSUMER_KEY', '');
-define('SECRET_KEY', '');
+class Yahoo_Messenger_Client {
+    
+    const CONSUMER_KEY = '';
+    const SECRET_KEY   = '';
+    
+    protected $jsonObj;
+    protected $engine;
+    
+    public function __construct($config){
+        $filename = "config/" . $config . ".json";
+        
+        $this->jsonObj = $this->jsonLoad($filename);
+        
+        $this->engine = new Yahoo_Messenger_Engine(self::CONSUMER_KEY, self::SECRET_KEY, $this->jsonObj->robot->credentials->username, $this->jsonObj->robot->credentials->password);
+        $this->engine->debug = true;
+    }
+    
+    private function jsonLoad($filename){
+        $handle = fopen($filename, "r");
+        $json = fread($handle, filesize($filename));
+        fclose($handle);
 
-require_once 'Yahoo/Messenger/Engine.php';
+        $jsonObj = json_decode($json);
 
-$engine = new Yahoo_Messenger_Engine(CONSUMER_KEY, SECRET_KEY, USERNAME, PASSWORD);
-$engine->debug = false;
+        return $jsonObj;
+    }
 
-if ($engine->debug) echo '> Fetching request token'. PHP_EOL;
-if (!$engine->fetchRequestToken()) die('Fetching request token failed');
+    public function getUsername(){
+        return $this->jsonObj->robot->credentials->username;
+    }
+    
+    public function connect(){
+        if ($this->engine->debug) {
+            echo '> Fetching request token'. PHP_EOL;
+        }
+        if (!$this->engine->fetchRequestToken()) {
+            $msg = "[" . $this->jsonObj->robot->credentials->username . "] " . 'Fetching request token failed';
+            die($msg);
+        }
+        
+        if ($this->engine->debug) { 
+            echo '> Fetching access token'. PHP_EOL;
+        }
+        
+        if (!$this->engine->fetchAccessToken()) {
+            $msg = "[" . $this->jsonObj->robot->credentials->username . "] " . 'Fetching access token failed';
+            die($msg);
+        }
+        
+        if ($this->engine->debug) {
+            echo '> Signon as: '. $this->jsonObj->robot->credentials->username . PHP_EOL;
+        }
+        
+        if (!$this->engine->signon($this->jsonObj->robot->presence_status, $this->jsonObj->robot->presence_state)) {
+            $msg = "[" . $this->jsonObj->robot->credentials->username . "] " . "Sigon failed 1";
+            die($msg);
+        }
+    }
+	public function run() { 
+	    
+        $seq = -1;
+        while (true)
+        {
+        	$resp = $this->engine->fetchLongNotification($seq+1);
+        	if (isset($resp))
+        	{	
+        		if ($resp === false) 
+        		{		
+        			if ($this->engine->getError() != -10)
+        			{
+        				if ($this->engine->debug) echo '> Fetching access token'. PHP_EOL;
+        				if (!$this->engine->fetchAccessToken()) die('Fetching access token failed');				
 
-if ($engine->debug) echo '> Fetching access token'. PHP_EOL;
-if (!$engine->fetchAccessToken()) die('Fetching access token failed');
+        				if ($this->engine->debug) echo '> Signon as: '. self::USERNAME. PHP_EOL;
+        				if (!$this->engine->signon(date('H:i:s'))) die('Signon failed');
 
-if ($engine->debug) echo '> Signon as: '. USERNAME. PHP_EOL;
-if (!$engine->signon('I am login from PHP code')) die('Signon failed');
+        				$seq = -1;
+        			}
+        			continue;							
+        		}
 
-$seq = -1;
-while (true)
-{
-	$resp = $engine->fetchLongNotification($seq+1);
-	if (isset($resp))
-	{	
-		if ($resp === false) 
-		{		
-			if ($engine->getError() != -10)
-			{
-				if ($engine->debug) echo '> Fetching access token'. PHP_EOL;
-				if (!$engine->fetchAccessToken()) die('Fetching access token failed');				
-				
-				if ($engine->debug) echo '> Signon as: '. USERNAME. PHP_EOL;
-				if (!$engine->signon(date('H:i:s'))) die('Signon failed');
-				
-				$seq = -1;
-			}
-			continue;							
-		}
-		
-		
-		foreach ($resp as $row)
-		{
-			foreach ($row as $key=>$val)
-			{
-				if ($val['sequence'] > $seq) $seq = intval($val['sequence']);
-				
-				/*
-				 * do actions
-				 */
-				if ($key == 'buddyInfo') //contact list
-				{
-					if (!isset($val['contact'])) continue;
-					
-					if ($engine->debug) echo PHP_EOL. 'Contact list: '. PHP_EOL;
-					foreach ($val['contact'] as $item)
-					{
-						if ($engine->debug) echo $item['sender']. PHP_EOL;
-					}
-					if ($engine->debug) echo '----------'. PHP_EOL;
-				}
-				
-				else if ($key == 'message') //incoming message
-				{
-					if ($engine->debug) echo '+ Incoming message from: "'. $val['sender']. '" on "'. date('H:i:s', $val['timeStamp']). '"'. PHP_EOL;
-					if ($engine->debug) echo '   '. $val['msg']. PHP_EOL;
-					if ($engine->debug) echo '----------'. PHP_EOL;
-					
-					//reply
-					$words = explode(' ', trim(strtolower($val['msg'])));
-					if ($words[0] == 'help')
-					{
-						$out = 'This is Yahoo! Open API demo'. PHP_EOL;
-						$out .= '  To get recent news from yahoo type: news'. PHP_EOL;
-						$out .= '  To get recent entertainment news from yahoo type: omg'. PHP_EOL;						
-						$out .= '  To change my/robot status type: status newstatus'. PHP_EOL;
-					}
-					else if ($words[0] == 'news')
-					{
-						if ($engine->debug) echo '> Retrieving news rss'. PHP_EOL;
-						$rss = file_get_contents('http://rss.news.yahoo.com/rss/topstories');
-												
-						if (preg_match_all('|<title>(.*?)</title>|is', $rss, $m))
-						{
-							$out = 'Recent Yahoo News:'. PHP_EOL;
-							for ($i=2; $i<7; $i++)
-							{
-								$out .= str_replace("\n", ' ', $m[1][$i]). PHP_EOL;
-							}
-						}
-					}
-					else if ($words[0] == 'omg')
-					{
-						if ($engine->debug) echo '> Retrieving OMG news rss'. PHP_EOL;
-						$rss = file_get_contents('http://rss.omg.yahoo.com/latest/news/');
-												
-						if (preg_match_all('|<title>(.*?)</title>|is', $rss, $m))
-						{
-							$out = 'Recent OMG News:'. PHP_EOL;
-							for ($i=2; $i<7; $i++)
-							{
-								$out .= str_replace(array('<![CDATA[', ']]>'), array('', ''), $m[1][$i]). PHP_EOL;
-							}
-						}
-					}	
-					else if ($words[0] == 'status')
-					{
-						$engine->changePresence(str_replace('status ', '', strtolower($val['msg'])));
-						$out = 'My status is changed';
-					}	
-					else
-					{
-						$out = 'Please type: help';
-					}
-					
-					//send message
-					if ($engine->debug) echo '> Sending reply message '. PHP_EOL;
-					if ($engine->debug) echo '    '. $out. PHP_EOL;	
-					if ($engine->debug) echo '----------'. PHP_EOL;
-					$engine->sendMessage($val['sender'], json_encode($out));
-				}
-				
-				else if ($key == 'buddyAuthorize') //incoming contact request
-				{
-					if ($engine->debug) echo PHP_EOL. 'Accept buddy request from: '. $val['sender']. PHP_EOL;					
-					if ($engine->debug) echo '----------'. PHP_EOL;	
-					if (!$engine->responseContact($val['sender'], true, 'Welcome to my list'))
-					{
-						$engine->deleteContact($val['sender']);
-						$engine->responseContact($val['sender'], true, 'Welcome to my list');
-					}
-				}
-		}
-		}
-	}	
+
+        		foreach ($resp as $row)
+        		{
+        			foreach ($row as $key=>$val)
+        			{
+        				if ($val['sequence'] > $seq) $seq = intval($val['sequence']);
+
+        				/*
+        				 * do actions
+        				 */
+        				if ($key == 'buddyInfo') //contact list
+        				{
+        					if (!isset($val['contact'])) continue;
+
+        					if ($this->engine->debug) echo PHP_EOL. 'Contact list: '. PHP_EOL;
+        					foreach ($val['contact'] as $item)
+        					{
+        						if ($this->engine->debug) echo $item['sender']. PHP_EOL;
+        					}
+        					if ($this->engine->debug) echo '----------'. PHP_EOL;
+        				}
+
+        				else if ($key == 'message') //incoming message
+        				{
+        					if ($this->engine->debug) echo '+ Incoming message from: "'. $val['sender']. '" on "'. date('H:i:s', $val['timeStamp']). '"'. PHP_EOL;
+        					if ($this->engine->debug) echo '   '. $val['msg']. PHP_EOL;
+        					if ($this->engine->debug) echo '----------'. PHP_EOL;
+
+        					//reply
+        					$words = explode(' ', trim(strtolower($val['msg'])));
+        					if ($words[0] == 'help')
+        					{
+        						$out = 'This is Yahoo! Open API demo'. PHP_EOL;
+        						$out .= '  To get recent news from yahoo type: news'. PHP_EOL;
+        						$out .= '  To change my/robot status type: status newstatus'. PHP_EOL;
+        					}
+        					else if ($words[0] == 'news')
+        					{
+        						if ($this->engine->debug) echo '> Retrieving news rss'. PHP_EOL;
+        						$rss = file_get_contents('http://rss.news.yahoo.com/rss/topstories');
+
+        						if (preg_match_all('|<title>(.*?)</title>|is', $rss, $m))
+        						{
+        							$out = 'Recent Yahoo News:'. PHP_EOL;
+        							for ($i=2; $i<7; $i++)
+        							{
+        								$out .= str_replace("\n", ' ', $m[1][$i]). PHP_EOL;
+        							}
+        						}
+        					}
+        					else if ($words[0] == 'status')
+        					{
+        						$this->engine->changePresence(str_replace('status ', '', strtolower($val['msg'])));
+        						$out = 'My status is changed';
+        					}	
+        					else if ($words[0] == 'presence_state')
+        					{
+        						$this->engine->changePresenceState(str_replace('presence_state ', '', strtolower($val['msg'])));
+        						$out = 'My status is changed';
+        					}
+        					else
+        					{
+        						$out = 'Please type: help';
+        					}
+
+        					//send message
+        					if ($this->engine->debug) echo '> Sending reply message '. PHP_EOL;
+        					if ($this->engine->debug) echo '    '. $out. PHP_EOL;	
+        					if ($this->engine->debug) echo '----------'. PHP_EOL;
+        					$this->engine->sendMessage($val['sender'], json_encode($out));
+        				}
+
+        				else if ($key == 'buddyAuthorize') //incoming contact request
+        				{
+        					if ($this->engine->debug) echo PHP_EOL. 'Accept buddy request from: '. $val['sender']. PHP_EOL;					
+        					if ($this->engine->debug) echo '----------'. PHP_EOL;	
+        					if (!$this->engine->responseContact($val['sender'], true, 'Welcome to my list'))
+        					{
+        						$this->engine->deleteContact($val['sender']);
+        						$this->engine->responseContact($val['sender'], true, 'Welcome to my list');
+        					}
+        				}
+        		}
+        		}
+        	}	
+	    }
+	}
 }
 
 ?>
